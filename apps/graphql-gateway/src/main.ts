a68d1773134fe8f7c67ae3bf567d4c3cbbccf26e
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-09-23 15:37:50 +0700

    hotfix: CORS

--- apps/graphql-gateway/src/main.ts
+++ apps/graphql-gateway/src/main.ts
@@ -7,6 +7,17 @@ async function bootstrap() {
     const port = process.env.PORT ?? 8000
     await app.listen(port)
 
+    app.enableCors({
+        // origin: [
+        //     "http://localhost:3000", // Thay bằng domain FE của bạn
+        //     "https://your-frontend-domain.com"
+        // ],
+        origin: "*",
+        credentials: true, // Cho phép gửi cookie, token
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+
     console.log(
         `🚀 GraphQL Gateway is running on: http://localhost:${port}/graphql`,
     )
