95661a3c554e500caa4b88b29c3912ae110e8cf2
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-11-05 17:01:03 +0700

    feat(user): add wallet and wallet transaction model

---
+++ apps/user/prisma/migrations/20251105095951_add_wallet_and_wallet_transaction/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "wallets" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "balance" BIGINT NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "wallets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "wallet_transactions" (
+    "id" TEXT NOT NULL,
+    "wallet_id" TEXT NOT NULL,
+    "campaign_id" TEXT NOT NULL,
+    "amount" BIGINT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "wallet_transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "wallets_user_id_key" ON "wallets"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "wallet_transactions_wallet_id_key" ON "wallet_transactions"("wallet_id");
+
+-- AddForeignKey
+ALTER TABLE "wallets" ADD CONSTRAINT "wallets_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "wallet_transactions" ADD CONSTRAINT "wallet_transactions_wallet_id_fkey" FOREIGN KEY ("wallet_id") REFERENCES "wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
