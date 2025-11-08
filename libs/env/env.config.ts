884251fd622a91ac445240c4d1af52636ee4dc97
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-11-08 15:02:41 +0700

    fix traceEnabled and logsInjection hardcode

--- libs/env/env.config.ts
+++ libs/env/env.config.ts
@@ -169,8 +169,8 @@ export const envConfig = (): EnvironmentConfig => ({
         agentPort: Number.parseInt(process.env.DD_AGENT_PORT || "8125"),
         env: process.env.DD_ENV || process.env.NODE_ENV || "development",
         version: process.env.DD_VERSION || "1.0.0",
-        logsInjection: true,
-        traceEnabled: true,
+        logsInjection: (process.env.DD_LOGS_INJECTION ?? "true") === "true",
+        traceEnabled: (process.env.DD_LOGS_INJECTION ?? "true") === "true",
         site: process.env.DD_SITE || "us5.datadoghq.com",
         traceSampleRate: Number.parseFloat(process.env.DD_TRACE_SAMPLE_RATE || "1.0"),
     }
